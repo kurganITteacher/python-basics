6195f92b75ff5cb595498f479d405f79048e6f10
Jo <jo@local>
2020-09-14 12:52:29 +0500

    lesson_dates_and_marks

--- 04.lists.py
+++ 04.lists.py
@@ -5,40 +5,34 @@ lesson_dates = [
     '19.05.19',
     '19.05.22'
 ]
-student_marks = [5, 4, 3, 2, 5]
+student_marks = [
+    5,
+    4,
+    3,
+    2,
+    5
+]
 
-# i = 0
-# while i < len(student_marks):
-#     print(lesson_dates[i], 'оценка', student_marks[i])
-#     i += 1
+lesson_dates_and_marks = [
+    ['19.05.15', 5],
+    ['19.05.17', 4],
+    ['19.05.18', 3],
+    ['19.05.19', 2],
+    ['19.05.22', 5],
+]
 
-# for item in student_marks:  # item
-#     print('оценка', item)
-#
-# for item in enumerate(student_marks):  # pairs -> (num, item)
-#     print('оценка', item)
+# for i, mark in enumerate(student_marks):
+#     print(lesson_dates[i], 'оценка', mark)
 
-for item in enumerate(student_marks):  # pairs -> (num, item)
-    i, mark = item
-    print('оценка', item, 'или', i, mark)
-
-# user_full_name = ['Иван', 'Иванов']
-# # first_name = user_full_name[0]
-# # second_name = user_full_name[1]
-#
-# # first_name, second_name = ['Иван', 'Иванов']
-# first_name, second_name = user_full_name
-#
-# print(first_name, second_name)
-#
-# a, b, c, d, e = [15, 45, 87, 96, 4]
-# print(a, b, c, d, e)
+# Union
+for record in lesson_dates_and_marks:
+    lesson_date, mark = record
+    # print(record, 'или', lesson_date, mark)
+    print(lesson_date, 'оценка', mark)
 
 
-# feature
-# in place
-for i, mark in enumerate(student_marks):  # pairs -> (num, mark)
-    print('оценка', i, mark)
-    # print(lesson_dates[i], 'оценка', item)
+for lesson_date, mark in lesson_dates_and_marks:
+    print(lesson_date, 'оценка', mark)
 
-# i, mark = item -> i, mark = (0, 5) -> i = 0, mark = 5
+
+
