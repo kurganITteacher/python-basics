1791e98a43ec2a2b90aba8c1b298084aa00f0978
Jo <jo@local>
2020-09-14 11:26:14 +0500

    mocks 2

--- 02.lists.py
+++ 02.lists.py
@@ -3,23 +3,22 @@
 # while True:
 #     mark = input('введите оценку студента:\n')
 #     if mark:
-#         student_marks.append(mark)
+#         student_marks.append(int(mark))
 #     else:
 #         break
 #
 # print('ввод завершен')
 # print(student_marks)
 
-mock_student_marks = ['5', '4', '3', '2', '5']  # correct mock
-# mock_student_marks = [5, 4, 3, 2, 5]  # wrong mock
+# mock_student_marks = ['5', '4', '3', '2', '5']  # wrong mock
+mock_student_marks = [5, 4, 3, 2, 5]  # correct mock
 student_marks = mock_student_marks
 
 # data processing
 i = 0
 avg_mark = 0
 while i < len(student_marks):
-    avg_mark += int(student_marks[i])
-    # avg_mark += student_marks[i]
+    avg_mark += student_marks[i]
     i += 1
 avg_mark /= len(student_marks)
 print('средний балл', avg_mark)
