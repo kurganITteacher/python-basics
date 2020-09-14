5f411bbd28dd94f7fef242149cc19ad382641aa9
Jo <jo@local>
2020-09-14 12:20:42 +0500

    for in

--- 02.lists.py
+++ 02.lists.py
@@ -15,10 +15,17 @@ mock_student_marks = [5, 4, 3, 2, 5]  # correct mock
 student_marks = mock_student_marks
 
 # data processing
-i = 0
+# i = 0
+# avg_mark = 0
+# while i < len(student_marks):
+#     avg_mark += student_marks[i]
+#     i += 1
+# avg_mark /= len(student_marks)
+# print('средний балл', avg_mark)
+
 avg_mark = 0
-while i < len(student_marks):
-    avg_mark += student_marks[i]
-    i += 1
+for item in student_marks:
+    avg_mark += item
+
 avg_mark /= len(student_marks)
 print('средний балл', avg_mark)
