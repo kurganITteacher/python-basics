a21619144f21f6bf15c98656d57569bc16af8b5d
Jo <jo@local>
2020-09-14 11:16:56 +0500

    mocks

--- 01.lists.py
+++ 01.lists.py
@@ -1,28 +1,23 @@
-student_marks = []
-while True:
-    mark = input('введите оценку студента:\n')
-    if mark:
-        student_marks.append(mark)
-    else:
-        break
+# student_marks = []
+# while True:
+#     mark = input('введите оценку студента:\n')
+#     if mark:
+#         student_marks.append(mark)
+#     else:
+#         break
+#
+# print('ввод завершен')
+# print(student_marks)
 
-print('ввод завершен')
-print(student_marks)
+mock_student_marks = ['5', '4', '3', '2', '5']
+# mock_student_marks = [5, 4, 3, 2, 5]
+student_marks = mock_student_marks
 
-# ['5', '4', '3', '5', '2']
-#   0    1    2    3    4
 i = 0
 avg_mark = 0
 while i < len(student_marks):
-    # print(type(avg_mark), type(student_marks[i]))
-    # avg_mark = avg_mark + int(student_marks[i])
     avg_mark += int(student_marks[i])
-    # i = i + 1
+    # avg_mark += student_marks[i]
     i += 1
-# avg_mark = avg_mark / len(student_marks)
 avg_mark /= len(student_marks)
 print('средний балл', avg_mark)
-
-# <num1> + <num2> - binary operation
-# -<num> - unary operation
-# 0 + '5' -> TypeError: unsupported operand type(s) for +: 'int' and 'str'
