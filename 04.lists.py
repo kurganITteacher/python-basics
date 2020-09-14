7e68d716237ab4ef2e0bd60f47b0a0cd60247e5b
Jo <jo@local>
2020-09-14 13:10:27 +0500

    zip 2

--- 04.lists.py
+++ 04.lists.py
@@ -13,27 +13,14 @@ student_marks = [
     5
 ]
 
-# lesson_dates_and_marks = [
-#     ['19.05.15', 5],
-#     ['19.05.17', 4],
-#     ['19.05.18', 3],
-#     ['19.05.19', 2],
-#     ['19.05.22', 5],
-# ]
-
-# for i, mark in enumerate(student_marks):
-#     print(lesson_dates[i], 'оценка', mark)
-
-# Union
-# for record in lesson_dates_and_marks:
-#     lesson_date, mark = record
-#     # print(record, 'или', lesson_date, mark)
-#     print(lesson_date, 'оценка', mark)
+student_2_marks = [
+    4,
+    3,
+    5,
+    5,
+    4
+]
 
 
-# for lesson_date, mark in lesson_dates_and_marks:
-#     print(lesson_date, 'оценка', mark)
-
-
-for lesson_date, mark in zip(lesson_dates, student_marks):  # ['19.05.15', 5]
-    print(lesson_date, 'оценка', mark)
+for lesson_date, mark, mark_2 in zip(lesson_dates, student_marks, student_2_marks):  # ['19.05.15', 5, 4]
+    print(lesson_date, 'оценка', 'студента 1', mark, ', студента 2', mark_2)
