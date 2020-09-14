e856d4c77c99765a34aaf3788c8ef4c49318038f
Jo <jo@local>
2020-09-14 13:24:23 +0500

    hometask

--- 04.lists.py
+++ 04.lists.py
@@ -23,4 +23,7 @@ student_2_marks = [
 
 
 for lesson_date, mark, mark_2 in zip(lesson_dates, student_marks, student_2_marks):  # ['19.05.15', 5, 4]
-    print(lesson_date, 'оценка', 'студента 1', mark, ', студента 2', mark_2)
+    # print(lesson_date, 'оценка', 'студента 1', mark, ', студента 2', mark_2)
+    print(lesson_date)
+
+curren_date = input('введите дату:\n')
