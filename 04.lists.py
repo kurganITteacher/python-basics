02942bcaaba5ee0c99a19d6cf78e17076d74cc11
Jo <jo@local>
2020-09-14 12:37:30 +0500

    enumerate

--- 04.lists.py
+++ 04.lists.py
@@ -12,5 +12,33 @@ student_marks = [5, 4, 3, 2, 5]
 #     print(lesson_dates[i], 'оценка', student_marks[i])
 #     i += 1
 
-for mark in student_marks:
-    print('оценка', mark)
+# for item in student_marks:  # item
+#     print('оценка', item)
+#
+# for item in enumerate(student_marks):  # pairs -> (num, item)
+#     print('оценка', item)
+
+for item in enumerate(student_marks):  # pairs -> (num, item)
+    i, mark = item
+    print('оценка', item, 'или', i, mark)
+
+# user_full_name = ['Иван', 'Иванов']
+# # first_name = user_full_name[0]
+# # second_name = user_full_name[1]
+#
+# # first_name, second_name = ['Иван', 'Иванов']
+# first_name, second_name = user_full_name
+#
+# print(first_name, second_name)
+#
+# a, b, c, d, e = [15, 45, 87, 96, 4]
+# print(a, b, c, d, e)
+
+
+# feature
+# in place
+for i, mark in enumerate(student_marks):  # pairs -> (num, mark)
+    print('оценка', i, mark)
+    # print(lesson_dates[i], 'оценка', item)
+
+# i, mark = item -> i, mark = (0, 5) -> i = 0, mark = 5
