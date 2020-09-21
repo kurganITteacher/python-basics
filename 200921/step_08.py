0390846362fb8f82bb5b267372fcfe53377ea534
Jo <jo@local>
2020-09-21 12:47:05 +0500

    lesson 200921    - after refactor

--- 200921/step_08.py
+++ 200921/step_08.py
@@ -2,9 +2,23 @@
 class_pupils = 'Полина,Антон, Арсений , Евгений,  Алексей, Тимур '
 correct_result = ['Полина', 'Антон', 'Арсений', 'Евгений', 'Алексей', 'Тимур']
 
-_result = class_pupils.split(',')
-result = []
-for item in _result:
-    result.append(item.strip())
+# _result = class_pupils.split(',')
+# result = []
+# for item in _result:
+#     result.append(item.strip())
 
+# result = list(map(str.strip, class_pupils.split(',')))
+# list comprehensions (bad idea - генераторы списков)
+result = [item.strip() for item in class_pupils.split(',')]
+
+# map
+# [
+#     'Полина', -> str.strip('Полина') -> 'Полина'
+#     'Антон', -> ...
+#     ' Арсений ', -> str.strip(' Арсений ') -> 'Арсений'
+#     ' Евгений', -> ...
+#     '  Алексей', ->
+#     ' Тимур ' ->
+# ]
+print(result)
 assert result == correct_result, 'алгоритм реализован неверно'
