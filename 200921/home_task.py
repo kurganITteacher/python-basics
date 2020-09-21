4f37e5be74e022ae45e45ec965814be6fde624fa
Jo <jo@local>
2020-09-21 13:25:50 +0500

    lesson 200921    - home task

---
+++ 200921/home_task.py
@@ -0,0 +1,7 @@
+'''
+217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"
+1. [<remote_IP_address>, <request_datetime>]
+*2. [<remote_IP_address>, <request_datetime>, <request_method>, <requested_resource>]
+['217.168.17.5', '17/May/2015:08:05:12 +0000', 'GET', '/downloads/product_2']
+3. print parsed log
+'''
