684bfc64130adccadf53635f085bb41eaf53cddc
Jo <jo@local>
2020-09-21 11:22:44 +0500

    lesson 200921    - try ... except    - .isdigit()    - refactor folders    - hw 2 example

---
+++ 200921/step_05.py
@@ -0,0 +1,32 @@
+lesson_dates = [
+    '19.05.15',
+    '19.05.17',
+    '19.05.18',
+    '19.05.19',
+    '19.05.22'
+]
+student_marks = [
+    5,
+    4,
+    3,
+    2,
+    5
+]
+
+student_2_marks = [
+    4,
+    3,
+    5,
+    5,
+    4
+]
+
+for lesson_date, mark, mark_2 in zip(lesson_dates, student_marks, student_2_marks):  # ['19.05.15', 5, 4]
+    # print(lesson_date, 'оценка', 'студента 1', mark, ', студента 2', mark_2)
+    print(lesson_date)
+
+curren_date = input('введите дату:\n')
+date_index = lesson_dates.index(curren_date)
+# date_index = 3
+# print(date_index, student_marks[date_index], student_2_marks[date_index])
+print('оценки студентов за', lesson_dates[date_index], ':', student_marks[date_index], student_2_marks[date_index])
