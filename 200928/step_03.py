8374a715b8e5d7c499da4db15bc6468223a467f0
Jo <carwash999@yandex.ru>
2020-09-28 11:30:39 +0500

    lesson 200928    - warm up    - readlines()    - f.close()    - context manager

---
+++ 200928/step_03.py
@@ -0,0 +1,11 @@
+# f = open('nginx_logs_head.txt', 'r', encoding='utf-8')
+
+# context manager
+# __enter__
+# __exit__ -> f.close()
+# transaction.atomic() -> restore point
+with open('nginx_logs_head.txt', 'r', encoding='utf-8') as f:
+    for row in f.read().splitlines():
+        print(row)
+
+# print(f.closed)
