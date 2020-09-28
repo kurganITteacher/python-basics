d95fd2f9b935927471b5b2d65378c8e732ca4084
Jo <carwash999@yandex.ru>
2020-09-28 12:56:55 +0500

    lesson 200928    - warm up    - readlines()    - f.close()    - context manager    - f.write()    - some fixes    - try    - def    - refactoring

---
+++ 200928/step_06.py
@@ -0,0 +1,36 @@
+# settings
+src_data_file = 'nginx_logs_head.txt'
+parsed_data_file = 'nginx_logs_parsed.csv'
+
+
+def row_parse(item):
+    remote_IP_address, row_tail = item.split(maxsplit=1)
+    _, _request_datetime = row_tail.split('[', maxsplit=1)
+    request_datetime, row_tail = _request_datetime.split(']', maxsplit=1)
+
+    _, _request_method, row_tail = row_tail.split('"', maxsplit=2)
+    request_method, requested_resource, _ = _request_method.split(maxsplit=2)
+
+    parsed_row = [remote_IP_address, request_datetime, request_method, requested_resource]
+    return list(map(str.strip, parsed_row))
+
+
+def file_parse(f_name: str) -> list:  # type annotations
+    parsed_data = []
+    with open(f_name, 'r', encoding='utf-8') as f:
+        for row in f.read().splitlines():
+            if not row:
+                continue
+            parsed_row = row_parse(row)
+            parsed_data.append(parsed_row)
+    return parsed_data
+
+
+def save_parsed_data(f_name, data):
+    with open(f_name, 'w', encoding='utf-8') as f:
+        for row in data:
+            f.write(f"{', '.join(row)}\n")
+
+
+parsed_data = file_parse(src_data_file)
+save_parsed_data(parsed_data_file, parsed_data)
