c90d724eba45c937be1857a046ae6064dab33b0a
Jo <carwash999@yandex.ru>
2020-10-05 11:53:26 +0500

    201005 lesson 2   - add head to csv   - dictionaries

--- 201005/students_stat.py
+++ 201005/students_stat.py
@@ -9,13 +9,35 @@ def parse_marks(f_name):
                 marks.append(int(mark.strip()))
             avg_mark = sum(marks) / len(marks)
             result.append([last_name, first_name, patronymic, marks, avg_mark])
+    return result
+
+
+def parse_marks_as_dict(f_name):
+    result = []
+    with open(f_name, 'r', encoding='utf-8') as f:
+        for row in f.read().splitlines():
+            last_name, first_name, patronymic, row_marks = row.split(maxsplit=3)
+            patronymic = patronymic.strip(',')
+            marks = []
+            for mark in row_marks.split(','):
+                marks.append(int(mark.strip()))
+            avg_mark = sum(marks) / len(marks)
+            result.append(
+                {
+                    'last_name': last_name,
+                    'first_name': first_name,
+                    'patronymic': patronymic,
+                    'marks': marks,
+                    'avg_mark': avg_mark
+                }
+            )
             # result.append(
             #     {
-            #         'last_name': last_name,
-            #         'first_name': first_name,
-            #         'patronymic': patronymic,
-            #         'marks': marks,
-            #         'avg_mark': avg_mark
+            #         0: last_name,
+            #         1: first_name,
+            #         2: patronymic,
+            #         3: marks,
+            #         4: avg_mark
             #     }
             # )
     return result
@@ -27,6 +49,18 @@ def show_marks(parsed_marks, raw=True, sep=' '):
             print(row)
         else:
             print(sep.join(map(str, row)))
+
+
+def show_students(parsed_marks):
+    for row in parsed_marks:
+        print(row[0], row[1], row[2])
+
+
+def show_students_dict(parsed_marks_as_dict):
+    for row in parsed_marks_as_dict:
+        print(row['first_name'], row['last_name'], row['patronymic'])
+        # print(row[0], row[1], row[2])
+
 
 
 def save_marks(f_name, parsed_marks):
