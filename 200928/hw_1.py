f33f2077fe14726bf00afef6a819fe4110043609
Jo <carwash999@yandex.ru>
2020-10-05 10:17:16 +0500

    201005 lesson 1   - hw 1   - hw 2

--- 200928/hw_1.py
+++ 200928/hw_1.py
@@ -2,8 +2,9 @@ with open('students_log.txt', 'r', encoding='utf-8') as f:
     for row in f.read().splitlines():
         last_name, first_name, patronymic, row_marks = row.split(maxsplit=3)
         patronymic = patronymic.strip(',')
-        # marks = list(map(str.strip, row_marks.split(',')))
+        # marks = list(map(int, map(str.strip, row_marks.split(','))))
         marks = []
         for mark in row_marks.split(','):
-            marks.append(mark.strip())
-        print(last_name, first_name, patronymic, ':', marks)
+            marks.append(int(mark.strip()))
+        avg_mark = sum(marks) / len(marks)
+        print(last_name, first_name, patronymic, ':', marks, ', средний балл', avg_mark)
