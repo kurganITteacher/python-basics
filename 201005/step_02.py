137fdcbf47c9d3835e96c183e958f07ef270a9bd
Jo <carwash999@yandex.ru>
2020-10-05 12:50:58 +0500

    201005 lesson 2   - add head to csv   - dictionaries   - json   - url parse (done)

--- 201005/step_02.py
+++ 201005/step_02.py
@@ -14,6 +14,22 @@ correct_answer = {
 
 answer = None
 
+trash, raw_get_data = concrete_request.split('?', maxsplit=1)
+# print(trash)
+# print(raw_get_data)
+get_data = raw_get_data.split('&')
+# print(get_data)
 
+parsed_get_data = []
+parsed_get_data_as_dict = {}
+for pair in get_data:
+    # print(pair.split('='))
+    tmp = pair.split('=')
+    parsed_get_data.append(tmp)
+    # print('key', tmp[0], 'value', tmp[1])
+    parsed_get_data_as_dict[tmp[0]] = tmp[1]
+print(parsed_get_data)
+print(parsed_get_data_as_dict)
+answer = parsed_get_data_as_dict
 
 assert answer == correct_answer, 'bad parser!'
