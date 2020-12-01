c72476809dfa0f143b2466c0093627821baa82ea
Jo <carwash999@yandex.ru>
2020-12-01 12:41:07 +0500

    step 2

--- 201201/step_1.py
+++ 201201/step_1.py
@@ -1,7 +1,10 @@
 # PEP-8
 # snake case
-my_var = 5
-print(my_var, type(my_var))
+my_var = 5  # instance class int
+print(my_var, type(my_var), dir(my_var))
 
-my_var = '5'
-print(my_var, type(my_var))
+my_var = '5'  # instance
+print(my_var, type(my_var), dir(my_var))
+
+my_var = 5.0  # instance
+print(my_var, type(my_var), dir(my_var))
---
+++ 201201/step_2.py
@@ -0,0 +1,7 @@
+my_var = '5'
+print(my_var, type(my_var))
+print(dir(my_var))
+
+# txt = 'Привет всем'
+# print(txt.upper())
+# print(txt.lower())
