b80ff86ba9f603d0363763d6551745dce3162194
Jo <carwash999@yandex.ru>
2020-12-01 13:13:37 +0500

    step 3

---
+++ 201201/step_3.py
@@ -0,0 +1,18 @@
+# pupil_1 = 'Данила'
+# pupil_2 = 'Илья'
+# pupil_3 = 'Георгий'
+#
+# print('группа 28:', pupil_1, pupil_2, pupil_3)
+
+# VS
+
+group_28 = ['Данила', 'Илья', 'Георгий']
+#               0        1        2
+print('группа 28:', group_28)
+print(group_28[0], group_28[2])
+print(type(group_28), dir(group_28))
+
+group_28.reverse()
+print(group_28)
+group_28.sort()
+print(group_28)
