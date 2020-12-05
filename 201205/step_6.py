13c92c75206a4f87229306b20e139f8c66d3bccb
Jo <carwash999@yandex.ru>
2020-12-05 11:32:43 +0500

    requests

--- 201205/step_6.py
+++ 201205/step_6.py
@@ -3,11 +3,16 @@ def calc_rectangle_area(w, h):
 
 
 def show_rectangle_area(area):
+    """Shows figure area
+
+    :param area:
+    :return:
+    """
     print('площадь прямоугольника', area)
 
 
-area = calc_rectangle_area(15, 20)
-show_rectangle_area(area)
+area_1 = calc_rectangle_area(15, 20)
+show_rectangle_area(area_1)
 
 show_rectangle_area(calc_rectangle_area(10, 30))
 
