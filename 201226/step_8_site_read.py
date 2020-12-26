6abf38e7e9cb75ad7289066265e378774ceac332
Jo <carwash999@yandex.ru>
2020-12-26 12:08:31 +0500

    kpk parse

---
+++ 201226/step_8_site_read.py
@@ -0,0 +1,14 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+}
+
+url = 'https://kpk.kss45.ru/%D1%83%D1%87%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0/%D1%80%D0%B0%D1%81%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BF%D0%B0%D1%80.html'
+
+response = requests.get(url, headers=headers)
+content = response.content.decode('utf-8')
+# print(content)
+
+with open('data/kpk.kss45.ru_timetable.txt', 'w', encoding='utf-8') as f:
+    f.write(content)
